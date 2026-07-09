a91fc2bf92cb930ade483ada611fbdcaa20a5068
DEVASRI G <gdevasri26@gmail.com>
2026-07-09 09:16:03 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:16:02 AM
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
