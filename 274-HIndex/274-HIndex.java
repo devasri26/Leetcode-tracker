3bddd91ad3f9e6e200325239aabe7b3ee49c4531
DEVASRI G <gdevasri26@gmail.com>
2026-07-09 09:15:02 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:15:01 AM
+class Solution {
+    public int hIndex(int[] citations) {
+          int n = citations.length;
+    int[] buckets = new int[n+1];
+    for(int c : citations) {
+        if(c >= n) {
+            buckets[n]++;
+        } else {
+            buckets[c]++;
+        }
+    }
+    int count = 0;
+    for(int i = n; i >= 0; i--) {
+        count += buckets[i];
+        if(count >= i) {
+            return i;
+        }
+    }
+    return 0;
+    }
+}
+        
